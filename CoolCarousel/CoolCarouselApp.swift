0dd136e989120471edd73e4b5a23d8c4ed8262d5
Vitor Trimer <vitortrimer@gmail.com>
2021-01-17 17:42:15 -0300

    Initial Commit

---
+++ CoolCarousel/CoolCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolCarouselApp.swift
+//  CoolCarousel
+//
+//  Created by Vitor Trimer on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CoolCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
